e12645d0ba765060537e0fc29c0bd7ee7d189ca2
Jonas lima <jonas.lima@sambatech.com.br>
2021-07-11 20:18:51 -0300

    corrigido erro no formulario

--- src/App.js
+++ src/App.js
@@ -14,10 +14,15 @@ function App() {
   return (
     <Container fluid>
       <Row className="cented-row">
-        <Col xl={4} lg={4} md={8} sm={12}className="offset-md-2 offset-xl-4 offset-lg-4">
-         <HorizontalFormIcons></HorizontalFormIcons>
+        <Col
+          xl={4}
+          lg={4}
+          md={8}
+          sm={12}
+          className="offset-md-2 offset-xl-4 offset-lg-4"
+        >
+          <HorizontalFormIcons />
         </Col>
-       
       </Row>
     </Container>
   );
